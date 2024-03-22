9e6d9368308f60766181037aceb88344c7f7f7de
BelExplorer <128513997+BelExplorer@users.noreply.github.com>
2024-03-21 23:07:59 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -10,3 +10,12 @@ cerrar.addEventListener("click", function() {
     navbar.classList.remove("visible");
 })
 
+for (var i = 0; i < 3; i++) {
+    document.querySelectorAll(".nav-menu-item") [i].addEventListener("click", function() {
+        navbar.classList.remove("visible");
+    })
+}
+
+document.querySelector("main").addEventListener("click", function() {
+    navbar.classList.remove("visible");
+})
